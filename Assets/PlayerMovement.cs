30748cd610acf6aa2f8a71375371acf6db2e9678
G-collab <galiatinvova@gmail.com>
2023-06-29 13:26:49 +0300

    Additional animatioan added falling+Jumping+idle+running is working properly

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -14,6 +14,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
     
+    private enum MovementState{ idle, running, jumping, falling}
     // Start is called before the first frame update
     void Start()
     {
@@ -50,19 +51,31 @@ public class PlayerMovement : MonoBehaviour
 
     private void UpdateAnimationState()
     {
+        MovementState state;
+        
         if (dirX > 0f)
         {
-            anim.SetBool("running", true);
+            state = MovementState.running;
             sprite.flipX = false;
         }
-        else if (dirX < 0F)
+        else if (dirX < 0f)
         {
-            anim.SetBool("running", true);
+            state = MovementState.running;
             sprite.flipX = true;
         }
         else
         {
-            anim.SetBool("running", false);
+            state = MovementState.idle;
+            if (rb.velocity.y > .1f)
+            {
+                state = MovementState.jumping;
+            }
+            else if (rb.velocity.y < -.1f)
+            {
+                state = MovementState.falling;
+            }
+
+            anim.SetInteger("state", (int) state);
         }
     }
         
